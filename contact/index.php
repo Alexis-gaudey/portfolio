5064fe9275d307c63dd1dd56b2176bb475f87c93
Alexis-gaudey <85607612+Alexis-gaudey@users.noreply.github.com>
2022-04-22 14:45:42 +0200

    Add files via upload

---
+++ contact/index.php
@@ -0,0 +1,7 @@
+<?php
+
+if(isset($_POST['formsend'])){
+    echo ""
+}
+
+?>
---
+++ contact/script.js
@@ -0,0 +1,22 @@
+
+document.forms["myform"].addEventListener("submit", function(e) {
+
+    var erreur;
+
+    var inputs = this;
+
+    for (var i = 0; i < inputs.length; i++) {
+        if (!inputs[i].value) {
+            erreur = "Veuillez renseigner tous les champs";
+        }
+    }
+    
+    if (erreur) {
+        e.preventDefault();
+        document.getElementById("erreur").innerHTML = erreur;
+        return false;
+    } else {
+        alert('Formulaire envoye !');
+    }
+
+});
